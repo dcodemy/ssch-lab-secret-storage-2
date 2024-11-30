c142800e3ede3297dc0dbd00759569e520860d4d
Davide Cioccia <daci@genmab.com>
2024-11-30 08:22:02 +0100

    deploy_contract fix

--- deploy_contract.sh
+++ deploy_contract.sh
@@ -35,6 +35,5 @@ ACCOUNT_ADDRESS=$(grep -o '0x[0-9a-fA-F]\{40\}' anvil_output.log | head -1)
 PRIVATE_KEY=$(grep -o '0x[0-9a-fA-F]\{64\}' anvil_output.log | head -1)
 
 SECRET=$(uuidgen)
-
 # Deploy the contract using Forge
 forge create --from $ACCOUNT_ADDRESS ./src/Vulnerable.sol:Hackme --constructor-args=$SECRET --unlocked
